0cc29a66b1b99bc7d0676c0e27078b3370da9272
Andrei Hava <andrei@omnilium.com>
2025-10-05 20:32:37 +0300

    add example mod

---
+++ Common.Mod.Example/ExampleSystem.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+using Vintagestory.API.Common;
+
+namespace Common.Mod.Example;
+
+[UsedImplicitly]
+public class ExampleSystem : ModSystem
+{
+    public override bool ShouldLoad(EnumAppSide forSide) => true;
+}
