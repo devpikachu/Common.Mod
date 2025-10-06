f27763b0a62edb4e903085180a2481dfce207c8c
Andrei Hava <andrei@omnilium.com>
2025-10-06 20:03:45 +0300

    advanced pipelines

--- .woodpecker/fetch-deps.sh
+++ .woodpecker/fetch-deps.sh
@@ -1,4 +1,8 @@
 set -e
 
+if [[ ! -d vendor ]]; then
+    mkdir vendor
+fi
+
 curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/protobuf-net.dll -o "vendor/protobuf-net.dll" -u "$1:$2"
 curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/VintagestoryAPI.dll -o "vendor/VintagestoryAPI.dll" -u "$1:$2"
