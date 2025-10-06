0c3f9ee346cb6a39f09304c2ade7eb6fa72098bc
Andrei Hava <andrei@omnilium.com>
2025-10-06 19:50:53 +0300

    fix pipelines

--- .woodpecker/fetch-deps.sh
+++ .woodpecker/fetch-deps.sh
@@ -1,6 +1,4 @@
 set -e
 
-mkdir -p "${VINTAGE_STORY}/Lib"
-
-curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/protobuf-net.dll -o "${VINTAGE_STORY}/Lib/protobuf-net.dll" -u "$1:$2"
-curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/VintagestoryAPI.dll -o "${VINTAGE_STORY}/VintagestoryAPI.dll" -u "$1:$2"
+curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/protobuf-net.dll -o "vendor/protobuf-net.dll" -u "$1:$2"
+curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/VintagestoryAPI.dll -o "vendor/VintagestoryAPI.dll" -u "$1:$2"
