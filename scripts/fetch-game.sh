5fadf54b47eac92126c35a430c2cbeb208188b3f
Andrei Hava <andrei@omnilium.com>
2025-10-06 20:49:10 +0300

    force file download

--- scripts/fetch-game.sh
+++ scripts/fetch-game.sh
@@ -4,5 +4,5 @@ if [[ ! -d vendor ]]; then
     mkdir vendor
 fi
 
-curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/protobuf-net.dll -o "vendor/protobuf-net.dll" -u "$1:$2"
-curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/VintagestoryAPI.dll -o "vendor/VintagestoryAPI.dll" -u "$1:$2"
+curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/protobuf-net.dll?raw -o "vendor/protobuf-net.dll" -u "$1:$2"
+curl -L https://files.omni.ms/protected/woodpecker/vintagestory/1.21.3/VintagestoryAPI.dll?raw -o "vendor/VintagestoryAPI.dll" -u "$1:$2"
